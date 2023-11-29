3dba56e12b24971fc94baefbfb7ee64e8f7d1893
Amy Cherrez <amy.cherrez@udla.edu.ec>
2023-11-29 17:08:25 -0500

    Se crea una carpeta AC_AboutPage.xaml y se agrega un codigo controlador en AC_AboutPage.xaml.cs

---
+++ AC_AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace ACherrez_AppApuntes;
+
+public partial class AC_AboutPage : ContentPage
+{
+	public AC_AboutPage()
+	{
+		InitializeComponent();
+	}
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
