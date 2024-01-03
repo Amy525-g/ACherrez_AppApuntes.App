c626ada8a1b5dffded4ee18b96724d98dd01df3e
Amy Cherrez <amy.cherrez@udla.edu.ec>
2024-01-03 16:08:11 -0500

    Se elimino dos archivos .cs de la carpeta Models, se agrego la carpeta ViewModels y tres clases se agregaron en la carpeta Views se cambio el codigo

---
+++ ViewModels/AC_NotesViewModel.cs
@@ -0,0 +1,56 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using ACherrez_AppApuntes.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ACherrez_AppApuntes.ViewModels;
+
+internal class AC_NotesViewModel: IQueryAttributable
+{
+    public ObservableCollection<ViewModels.AC_NoteViewModel> AllNotes { get; }
+    public ICommand NewCommand { get; }
+    public ICommand SelectNoteCommand { get; }
+
+    public AC_NotesViewModel()
+    {
+        AllNotes = new ObservableCollection<ViewModels.AC_NoteViewModel>(Models.AC_Note.LoadAll().Select(n => new AC_NoteViewModel(n)));
+        NewCommand = new AsyncRelayCommand(NewNoteAsync);
+        SelectNoteCommand = new AsyncRelayCommand<ViewModels.AC_NoteViewModel>(SelectNoteAsync);
+    }
+
+    private async Task NewNoteAsync()
+    {
+        await Shell.Current.GoToAsync(nameof(Views.AC_NotePage));
+    }
+
+    private async Task SelectNoteAsync(ViewModels.AC_NoteViewModel note)
+    {
+        if (note != null)
+            await Shell.Current.GoToAsync($"{nameof(Views.AC_NotePage)}?load={note.Identifier}");
+    }
+    void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("deleted"))
+        {
+            string noteId = query["deleted"].ToString();
+            AC_NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+            // If note exists, delete it
+            if (matchedNote != null)
+                AllNotes.Remove(matchedNote);
+        }
+        else if (query.ContainsKey("saved"))
+        {
+            string noteId = query["saved"].ToString();
+            AC_NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+            // If note is found, update it
+            if (matchedNote != null)
+                matchedNote.Reload();
+
+            // If note isn't found, it's new; add it.
+            else
+                AllNotes.Add(new AC_NoteViewModel(AC_Note.Load(noteId)));
+        }
+    }
+}
--- Views/AC_NotePage.xaml.cs
+++ Views/AC_NotePage.xaml.cs
@@ -1,55 +1,9 @@
 namespace ACherrez_AppApuntes.Views;
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
 
 public partial class AC_NotePage : ContentPage
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
-    public string ItemId
-    {
-        set { LoadNote(value); }
-    }
     public AC_NotePage()
-	{
-		InitializeComponent();
-        //if (File.Exists(_fileName))
-          //  TextEditor.Text = File.ReadAllText(_fileName);
-        string appDataPath = FileSystem.AppDataDirectory;
-        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
-    }
-
-    private async void SaveButton_Clicked(object sender, EventArgs e)
     {
-        if (BindingContext is Models.AC_Note note)
-            File.WriteAllText(note.Filename, TextEditor.Text);
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.AC_Note note)
-        {
-            // Delete the file.
-            if (File.Exists(note.Filename))
-                File.Delete(note.Filename);
-        }
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private void LoadNote(string fileName)
-    {
-        Models.AC_Note noteModel = new Models.AC_Note();
-        noteModel.Filename = fileName;
-
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
-
-        BindingContext = noteModel;
+        InitializeComponent();
     }
 }
