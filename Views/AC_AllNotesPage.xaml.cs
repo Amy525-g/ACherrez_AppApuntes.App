d10a80c278ce632504cfb0c07566c738944176db
Amy Cherrez <amy.cherrez@udla.edu.ec>
2024-01-03 16:20:31 -0500

    en la vista AC_AllNotes ahora se les permite seleccionar una nota dos veces

--- Views/AC_AllNotesPage.xaml.cs
+++ Views/AC_AllNotesPage.xaml.cs
@@ -6,4 +6,8 @@ public partial class AC_AllNotesPage : ContentPage
 	{
 		InitializeComponent();
     }
+    private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
+    {
+        notesCollection.SelectedItem = null;
+    }
 }
